0c9750382412b65313b0924cc4e43c4823dcdaae
lucasgcc <111548573+lucasgcc@users.noreply.github.com>
2022-11-22 21:05:39 -0300

    Add files via upload

---
+++ database/bd_connect.js
@@ -0,0 +1,45 @@
+const Connection = require('tedious').Connection;
+//const Request = require('tedious').Request;
+const TYPES = require('tedious').TYPES;
+
+const config = require ('./bdconfig.json')
+const connection = new Connection(config);
+
+connection.on('connect', function(err) {
+    if (err){
+        console.log(err)
+    }else {
+        console.log("Connected")
+        executeSQL()
+    }
+});
+
+connection.connect();
+
+function executeSQL() {
+    request = new Request("SELECT * FROM TIME.Nome", function(err) {
+    if (err) {
+        console.log(err);}
+    });
+    let result = "";
+    request.on('row', function(columns) {
+        columns.forEach(function(column) {
+          if (column.value === null) {
+            console.log('NULL');
+          } else {
+            result+= column.value + " ";
+          }
+        });
+        console.log(result);
+        result ="";
+    });
+
+    request.on('done', function(rowCount, more) {
+    console.log(rowCount + ' rows returned');
+    });
+    
+    request.on("requestCompleted", function (rowCount, more) {
+        connection.close();
+    });
+    connection.execSql(request);
+}
